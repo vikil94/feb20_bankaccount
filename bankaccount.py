d010a0e7c8f50400453ab85859ef3d2d68a644c9
Vikil Naik <vikilnaik37@gmail.com>
2019-02-21 15:18:20 -0500

    used linting to clean up my code

--- bankaccount.py
+++ bankaccount.py
@@ -21,29 +21,35 @@ class BankAccount:
     @classmethod
     def total_funds(cls):
         total = 0
-        for num in range(0,len(cls.accounts)): #cls.accounts refers to the list accounts
-            total += cls.accounts[num].balance # it is a class variable so needs the cls
-        return total                           # in order to call on that variable 
+        # cls.accounts refers to the list accounts
+        for num in range(0, len(cls.accounts)):
+            # it is a class variable so needs the cls
+            total += cls.accounts[num].balance
+            # in order to call on that variable
+        return total
+
 
     @classmethod
     def interest_time(cls):
-        for num in range(0,len(cls.accounts)):
+        for num in range(0, len(cls.accounts)):
             current_account = cls.accounts[num]
-            current_account.balance = current_account.balance * (1 + current_account.interest_rate / 100)
+            current_account.balance = current_account.balance
+            * (1 + current_account.interest_rate / 100)
+
 
 my_account = BankAccount.create()
 your_account = BankAccount.create()
-print(my_account.balance) # 0
-print(BankAccount.total_funds()) # 0
+print(my_account.balance)  # 0
+print(BankAccount.total_funds())  # 0
 my_account.deposit(200)
 your_account.deposit(1000)
-print(my_account.balance) # 200
-print(your_account.balance) # 1000
-print(BankAccount.total_funds()) # 1200
+print(my_account.balance)  # 200
+print(your_account.balance)  # 1000
+print(BankAccount.total_funds())  # 1200
 BankAccount.interest_time()
-print(my_account.balance) # 202.0
-print(your_account.balance) # 1010.0
-print(BankAccount.total_funds()) # 1212.0
+print(my_account.balance)  # 202.0
+print(your_account.balance)  # 1010.0
+print(BankAccount.total_funds())  # 1212.0
 my_account.withdraw(50)
-print(my_account.balance) # 152.0
-print(BankAccount.total_funds()) # 1162.0
+print(my_account.balance)  # 152.0
+print(BankAccount.total_funds())  # 1162.0
