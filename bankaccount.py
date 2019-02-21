6d924a228092e3925b7f7268ebb124733441ea2a
Vikil Naik <vikilnaik37@gmail.com>
2019-02-21 15:20:15 -0500

    Cleaned up my code by linting and using flake8

--- bankaccount.py
+++ bankaccount.py
@@ -37,19 +37,19 @@ class BankAccount:
             * (1 + current_account.interest_rate / 100)
 
 
-my_account = BankAccount.create()
-your_account = BankAccount.create()
-print(my_account.balance)  # 0
-print(BankAccount.total_funds())  # 0
-my_account.deposit(200)
-your_account.deposit(1000)
-print(my_account.balance)  # 200
-print(your_account.balance)  # 1000
-print(BankAccount.total_funds())  # 1200
-BankAccount.interest_time()
-print(my_account.balance)  # 202.0
-print(your_account.balance)  # 1010.0
-print(BankAccount.total_funds())  # 1212.0
-my_account.withdraw(50)
-print(my_account.balance)  # 152.0
-print(BankAccount.total_funds())  # 1162.0
+# my_account = BankAccount.create()
+# your_account = BankAccount.create()
+# print(my_account.balance)  # 0
+# print(BankAccount.total_funds())  # 0
+# my_account.deposit(200)
+# your_account.deposit(1000)
+# print(my_account.balance)  # 200
+# print(your_account.balance)  # 1000
+# print(BankAccount.total_funds())  # 1200
+# BankAccount.interest_time()
+# print(my_account.balance)  # 202.0
+# print(your_account.balance)  # 1010.0
+# print(BankAccount.total_funds())  # 1212.0
+# my_account.withdraw(50)
+# print(my_account.balance)  # 152.0
+# print(BankAccount.total_funds())  # 1162.0
